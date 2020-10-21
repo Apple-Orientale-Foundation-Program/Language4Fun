e01243c0a449aeb19c34f4a92d957490812dcef7
Giovanni Monaco <mon.giovanni@gmail.com>
2020-10-21 11:53:23 +0200

    Initial Commit

---
+++ Language4Fun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Language4Fun
+//
+//  Created by Giovanni Monaco on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Language4Fun/Language4FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language4FunApp.swift
+//  Language4Fun
+//
+//  Created by Giovanni Monaco on 21/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Language4FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
