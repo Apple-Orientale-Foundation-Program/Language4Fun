3e5db8a86762901e1bfe312f0eb19ed4aaec322b
Fabio Striano <fstriano@hotmail.com>
2020-10-23 18:10:05 +0200

    fixare OpenDyslexic

--- Language4Fun/Level1ScreenLetterView.swift
+++ Language4Fun/Level1ScreenLetterView.swift
@@ -8,15 +8,26 @@
 import SwiftUI
 
 struct Level1ScreenLetterView: View {
+    let customfont = Font.custom("OpenDyslexic3Regular.ttf", size: 255)
     var body: some View {
+        ZStack {
         Image("sfondo blu")
             .resizable()
             .ignoresSafeArea(.all)
+            
+            Text("A")
+                .font(customfont)
+                .foregroundColor(Color(red: 250 / 255, green: 191 / 255, blue: 170 / 255))
+                
+            
+        }
     }
 }
 
 struct Level1ScreenLetterView_Previews: PreviewProvider {
     static var previews: some View {
-        Level1ScreenLetterView()
+            Level1ScreenLetterView()
+            
+        
     }
 }
