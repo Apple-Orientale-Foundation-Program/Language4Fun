316d09cc6c98a17499edfd68d099f1b70f432b70
Fabio Striano <fstriano@hotmail.com>
2020-10-23 14:11:36 +0200

    levl1

---
+++ Language4Fun/Level1View.swift
@@ -0,0 +1,20 @@
+//
+//  Level1View.swift
+//  Language4Fun
+//
+//  Created by Fabio Striano on 23/10/2020.
+//
+
+import SwiftUI
+
+struct Level1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Level1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Level1View()
+    }
+}
