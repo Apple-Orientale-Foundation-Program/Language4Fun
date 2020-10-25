a6e00e6c0c105ad190779affe0108f554f2a23cf
Fabio Striano <fstriano@hotmail.com>
2020-10-25 10:56:18 +0100

    1st level 1st part

---
+++ Language4Fun/Level1ActivityView.swift
@@ -0,0 +1,20 @@
+//
+//  Level1ActivityView.swift
+//  Language4Fun
+//
+//  Created by Fabio Striano on 25/10/2020.
+//
+
+import SwiftUI
+
+struct Level1ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Level1ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        Level1ActivityView()
+    }
+}
--- Language4Fun/Level1ScreenLetterView.swift
+++ Language4Fun/Level1ScreenLetterView.swift
@@ -9,22 +9,48 @@ import SwiftUI
 
 struct Level1ScreenLetterView: View {
     var body: some View {
-        ZStack {
+        NavigationView{
+            NavigationLink(
+                destination: Level1ActivityView(),
+                label: {
+            ZStack {
             Image("sfondo blu")
                 .resizable()
                 .ignoresSafeArea(.all)
-            Text("Ciao a tutti e fanculo").font(Font.custom("OpenDyslexic3", size: 175))
+                
+            Text("A").font(Font.custom("OpenDyslexic3", size: 355))
             .foregroundColor(Color(red: 250 / 255, green: 191 / 255, blue: 170 / 255))
+                .position(x: 530, y: 350)
             
+            
+            Text("Tap to start").font(Font.custom("OpenDyslexic3", size: 55))
+            .foregroundColor(Color(red: 250 / 255, green: 191 / 255, blue: 170 / 255))
+                .position(x: 530, y: 950)
+                
+            HStack {
+            Text("Apple").font(Font.custom("OpenDyslexic3", size: 130))
+                .position(x: 430, y: 700)
+            .foregroundColor(Color(red: 250 / 255, green: 191 / 255, blue: 170 / 255))
+                
+                Image("A")
+                    .position(x: 250, y: 700)
+            }
             
         }
+    })
+}
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
-
 struct Level1ScreenLetterView_Previews: PreviewProvider {
     static var previews: some View {
         Level1ScreenLetterView()
+            .previewDevice("iPad Pro (12.9-inch) (4th generation)")
         
         
     }
 }
+
